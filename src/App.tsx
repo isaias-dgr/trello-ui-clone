cda722f41929a03c8570ccb0cf12ab68fcfafac3
Isaias <isaiasdanielgarciarodriguez@Mac-mini-de-Isaias.local>
2024-08-12 09:47:59 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/SideBar/SideBar";
+import SidebarItem from "./components/SideBar/SideBarItem";
+import Home from "./pages/Home";
+import { LayoutDashboard, ListTodo, Settings } from "lucide-react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App flex flex-grow overflow-hidden bg-slate-100">
+      <Sidebar>
+        <SidebarItem icon={<ListTodo size={20} />} text="ToDo List" alert />
+        <SidebarItem icon={<LayoutDashboard size={20} />} text="Board" active />
+        <hr className="my-3" />
+        <SidebarItem icon={<Settings size={20} />} text="Config" />
+      </Sidebar>
+      <Home />
+    </main>
   );
-}
+};
 
 export default App;
